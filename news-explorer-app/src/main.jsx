397e8602bae9ffa1fe23308f88d9eab5041e06d4
zmarks00 <122951066+zmarks00@users.noreply.github.com>
2026-05-27 13:07:46 -0400

    fixed loading issues

--- news-explorer-app/src/main.jsx
+++ news-explorer-app/src/main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/se_final_project">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
