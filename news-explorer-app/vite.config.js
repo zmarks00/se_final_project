880f2353d5168366ad92995a4ac79c0630dcc656
zmarks00 <122951066+zmarks00@users.noreply.github.com>
2026-05-27 13:08:00 -0400

    fixed loading issues

--- news-explorer-app/vite.config.js
+++ news-explorer-app/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/se_final_project/',
+  base: "/se_final_project/",
   plugins: [react()],
   server: {
     port: 3000,
